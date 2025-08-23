03fddae4773fd6b007b2324edda6a5d9c11e5bb8
Pamela Sezana <psezana@binario.cloud>
2025-08-23 12:27:24 -0300

    model Car created

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):  # Nome da tabela no banco de dados
+    id = models.AutoField(primary_key=True)  
+    # Campo ID, chave primária, autoincrementado automaticamente pelo Django
+
+    model = models.CharField(max_length=200)  
+    # Campo de texto (string), aceita até 200 caracteres
+
+    brand = models.CharField(max_length=200)  
+    # Marca do carro, também um campo de texto
+
+    factory_year = models.IntegerField(blank=True, null=True)  
+    # Ano de fabricação (inteiro), pode ser deixado em branco ou nulo
+
+    model_year = models.IntegerField(blank=True, null=True)  
+    # Ano do modelo (inteiro), pode ser deixado em branco ou nulo
+
+    value = models.FloatField(blank=True, null=True)  
+    # Valor do carro (número decimal), pode ser deixado em branco ou nulo
+
+
